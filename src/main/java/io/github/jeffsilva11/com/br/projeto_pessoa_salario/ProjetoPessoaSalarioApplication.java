f771dbcce6bcc5e7a6b8dead3879c93c3e8c0d7a
Jefferson Silva <fj_adm@hotmail.com>
2023-09-04 23:53:41 -0300

    Configuração de Banco de Dados Postgres

--- src/main/java/io/github/jeffsilva11/com/br/projeto_pessoa_salario/ProjetoPessoaSalarioApplication.java
+++ src/main/java/io/github/jeffsilva11/com/br/projeto_pessoa_salario/ProjetoPessoaSalarioApplication.java
@@ -8,6 +8,8 @@ public class ProjetoPessoaSalarioApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoPessoaSalarioApplication.class, args);
+		System.out.println("PROGRAMA INICIADO");
+		
 	}
 
 }
