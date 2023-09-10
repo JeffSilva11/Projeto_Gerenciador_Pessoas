493f8fd95a151194a5169d943c873054128e72a2
Jefferson Silva <fj_adm@hotmail.com>
2023-09-09 22:10:28 -0300

    Adicionando a Dependência para o Model Mapper

--- src/main/java/io/github/jeffsilva11/com/br/projeto_pessoa_salario/ProjetoPessoaSalarioApplication.java
+++ src/main/java/io/github/jeffsilva11/com/br/projeto_pessoa_salario/ProjetoPessoaSalarioApplication.java
@@ -1,11 +1,18 @@
 package io.github.jeffsilva11.com.br.projeto_pessoa_salario;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ProjetoPessoaSalarioApplication {
 
+	@Bean
+	public ModelMapper modelMapper(){
+			return new ModelMapper();
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoPessoaSalarioApplication.class, args);
 		System.out.println("PROGRAMA INICIADO");
